85b9dc666779976eb918cfa11a1049b5a03ed8a0
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-11-13 09:27:56 -0300

    alteração no controller do Chat Economia Energetica

--- src/main/java/br/com/ecometric/controller/ChatEconomiaEnergeticaController.java
+++ src/main/java/br/com/ecometric/controller/ChatEconomiaEnergeticaController.java
@@ -1,21 +1,27 @@
 package br.com.ecometric.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.ecometric.service.ChatEconomiaEnergeticaService;
 
-@RestController
-@RequestMapping("/chatEconomiaEnergetica")
+@Controller
 public class ChatEconomiaEnergeticaController {
 
     @Autowired
     private ChatEconomiaEnergeticaService chatService;
 
-    @PostMapping("/ask")
+    @GetMapping("/chatEconomiaEnergetica")
+    public String showChatPage() {
+        return "chatEconomiaEnergetica"; 
+    }
+
+    @PostMapping("/chatEconomiaEnergetica/ask")
+    @ResponseBody
     public String askQuestion(@RequestParam("question") String question) {
         return chatService.getAnswerFromAi(question);
     }
