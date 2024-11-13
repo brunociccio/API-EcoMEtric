9dbafe06eaeedd66036fad3b924c5169e843c2e1
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-11-13 08:55:15 -0300

    alteração do GetMapping da classe de Login teste

--- src/main/java/br/com/ecometric/controller/LoginPageController.java
+++ src/main/java/br/com/ecometric/controller/LoginPageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginPageController {
 
-    @GetMapping("/login-login")
+    @GetMapping("/custom-login")
     public String loginPage() {
         return "login"; 
     }
