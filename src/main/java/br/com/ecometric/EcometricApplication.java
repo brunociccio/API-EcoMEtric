c0ce3921e4b69b4a930077bf60f3075cdb197420
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-11-18 12:38:11 -0300

    Refatoração do EcometricApplication

--- src/main/java/br/com/ecometric/EcometricApplication.java
+++ src/main/java/br/com/ecometric/EcometricApplication.java
@@ -2,31 +2,17 @@ package br.com.ecometric;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @Controller
 public class EcometricApplication {
 
     public static void main(String[] args) {
-        // Inicializa Dotenv para carregar variáveis de ambiente do arquivo .env
-        Dotenv dotenv = Dotenv.configure()
-                .directory(".") // Diretório do arquivo .env
-                .filename(".env") // Nome do arquivo .env
-                .load();
-
+        // Inicializa a aplicação Spring Boot sem carregar o arquivo .env
         SpringApplication.run(EcometricApplication.class, args);
-    }
-
-    @Bean
-    public Dotenv dotenv() {
-        // Disponibiliza Dotenv como Bean no contexto Spring
-        return Dotenv.load();
     }
 
     @RequestMapping("/home")
@@ -35,3 +21,4 @@ public class EcometricApplication {
         return "API Full-Stack do Projeto EcoMetric";
     }
 }
+
